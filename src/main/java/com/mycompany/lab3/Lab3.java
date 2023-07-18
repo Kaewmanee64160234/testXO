7179974b792357efc76454ff49bc90723b2bd08e
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-07-18 21:59:11 +0700

    📝 create func testNoPlay

--- src/main/java/com/mycompany/lab3/Lab3.java
+++ src/main/java/com/mycompany/lab3/Lab3.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.lab3;
 
 /**
@@ -9,6 +8,77 @@ package com.mycompany.lab3;
  * @author USER
  */
 public class Lab3 {
+
+
+
+    public static boolean checkRow(String[][] board,int row,int column,String turn) {
+        for (int i = 0; i < 3; i++) {
+            if (!board[row - 1][i].equals(turn)) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public static boolean checkColumn(String[][] board,int row,int column,String turn) {
+        for (int i = 0; i < 3; i++) {
+            if (!board[i][column - 1].equals(turn)) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public static boolean checkDiagonal(String[][] board,int row,int column,String turn) {
+        if (row - 1 == column - 1) {
+            for (int i = 0; i < 3; i++) {
+                if (!board[i][i].equals(turn)) {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+        if ((row + column) - 2 == 2) {
+            for (int i = 0; i < 3; i++) {
+                if (!board[i][2 - i].equals(turn)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkDraw(String[][] board,int row,int column,String turn) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j].equals("-")) {
+                    return false;
+                }
+
+            }
+
+        }
+        return true;
+    }
+
+    public static boolean checkWinner(String[][] board,int row,int column,String turn) {
+
+        if (checkRow(board, row, column, turn) || checkColumn(board, row, column, turn) || checkDiagonal( board, row, column, turn)) {
+            System.out.println("--------------------------------");
+
+            System.out.println("+--------------------+");
+            System.out.println("|    !!! " + turn.toUpperCase() + " Win !!!   |");
+            System.out.println("+--------------------+");
+            return true;
+
+        }
+        return false;
+
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
--- src/test/java/XoTest.java
+++ src/test/java/XoTest.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 
+import com.mycompany.lab3.Lab3;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,23 +16,22 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author USER
  */
 public class XoTest {
-    
+
     public XoTest() {
     }
 
-  
     @BeforeAll
     public static void setUpClass() {
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
     public void setUp() {
     }
-    
+
     @AfterEach
     public void tearDown() {
     }
@@ -41,4 +41,13 @@ public class XoTest {
     //
     // @Test
     // public void hello() {}
+    @Test
+    public void test_Not_Play() {
+        String trun = "X";
+        String[][] board = {{"-", "-", "-"}, {"-", "-", "-"}, {"-", "-", "-"}};
+
+        assertEquals(false, Lab3.checkWinner(board, 1, 1, trun));
+
+    }
+    
 }
