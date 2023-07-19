10229d88f84c6956a3fcf4eb865137102ad3b2f4
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-07-19 14:24:46 +0700

    📝 create func Check_Win_When_O_Win_At_Row()

--- src/test/java/XoTest.java
+++ src/test/java/XoTest.java
@@ -90,13 +90,13 @@ public class XoTest {
         assertEquals(true, Lab3.checkWinner(board, row, column, turn));
     }
     
-//     @Test
-//    public void Check_Win_When_O_Win_At_Row() {
-//        String[][] board = {{"O", "O", "O"}, {"-", "-", "-"}, {"-", "-", "-"}};
-//        String turn = "O";
-//        int row = 1;
-//        int column = 1;
-//        assertEquals(true, Lab3.checkWinner(board, row, column, turn));
-//    }
+     @Test
+    public void Check_Win_When_O_Win_At_Row() {
+        String[][] board = {{"O", "O", "O"}, {"-", "-", "-"}, {"-", "-", "-"}};
+        String turn = "O";
+        int row = 1;
+        int column = 1;
+        assertEquals(true, Lab3.checkWinner(board, row, column, turn));
+    }
 
 }
